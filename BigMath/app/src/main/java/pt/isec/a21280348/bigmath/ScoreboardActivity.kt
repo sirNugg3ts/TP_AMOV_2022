3958245d763937691bb18f5a2de0aa9c065218a5
Leonardo <leodgsousa@gmail.com>
2022-12-24 02:52:34 +0000

    Firebase/store (writing the username, image and score in firestore).

--- BigMath/app/src/main/java/pt/isec/a21280348/bigmath/ScoreboardActivity.kt
+++ BigMath/app/src/main/java/pt/isec/a21280348/bigmath/ScoreboardActivity.kt
@@ -1,7 +1,10 @@
 package pt.isec.a21280348.bigmath
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -13,13 +16,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.AggregateSource
 import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 import pt.isec.a21280348.bigmath.databinding.ActivityScoreboardBinding
 import java.util.Objects
 
 class ScoreboardActivity : AppCompatActivity() {
-    data class ScoreData(val userName : String, val score : Long, val bitmap : String)
+    data class ScoreData(val userName : String, val score : Long, val imgBaseStr : String)
 
     lateinit var binding : ActivityScoreboardBinding
 
@@ -30,7 +34,14 @@ class ScoreboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityScoreboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //val score = intent.getIntExtra("score",5)
+        val sharedPref = this.getSharedPreferences(getString(R.string.user_file_key), Context.MODE_PRIVATE) ?: return
+        var insertedUser = false
+
+        //read user data
+        val playName = sharedPref.getString(getString(R.string.usernameIdent), "")
+        val playImage= sharedPref.getString(getString(R.string.imageIdent), "")
+        val playScore = intent.getIntExtra("score",5).toLong()
+        //Log.i("SCORE",playScore.toString())
 
 
 
@@ -45,25 +56,71 @@ class ScoreboardActivity : AppCompatActivity() {
 
         val myAdapter = RVAdapter(highScores)
         binding.scoreList.adapter = myAdapter
-        for (i in 5 downTo 1 ) {
+        /*for (i in 1..5 ) {
             val v = db_highscores.document("PointsScore_"+i.toString())
+            Log.i("Leaderboard", ("Lendo : PointsScore_"+i.toString()))
+            Thread.sleep(300)
             v.get(Source.SERVER).addOnSuccessListener {
                 val exists = it.exists()
                 if(!exists)
                     return@addOnSuccessListener
-                if(it.get("username") == null)
-                    return@addOnSuccessListener
-                username = it.getString("username") ?: "user"
-                score = it.getLong("score") ?: 0
-                image = it.getString("image") ?: "user"
+                if(it.get("username") == null){
+                    if(insertedUser)
+                        return@addOnSuccessListener
+                    else{
+                        insertedUser = true
+                    }
+                }
+                username = it.getString("username") ?: playName!!
+                score = it.getLong("score") ?: playScore
+                image = it.getString("image") ?: playImage!!
                 //v.update("username",newStr)
                 Log.i("Firebase", username)
                 Log.i("Firebase", score.toString())
                 Log.i("Firebase", image)
 
+                Log.i("Leaderboard",score.toString())
                 highScores.add(ScoreData(username,score,image))
-                myAdapter.notifyItemInserted(5-i)
+                myAdapter.notifyItemInserted(i-1)
             }
+        }*/
+        Log.i("NEWWAY", "start of")
+        db.collection("PointHighScores").get().addOnSuccessListener { documents->
+            var i = 0
+            for(document in documents){
+
+                if(document.get("username") == null){
+                    if(insertedUser)
+                        return@addOnSuccessListener
+                    else{
+                        val insertData = hashMapOf(
+                            "image" to playImage,
+                            "score" to playScore,
+                            "username" to playName,
+                        )
+                        insertedUser = true
+                        document.reference.set(insertData)
+                        highScores.add(ScoreData(playName!!, playScore, playImage!!))
+                        myAdapter.notifyItemInserted(i++)
+                    }
+                }else {
+
+                    username = document.getString("username") ?: playName!!
+                    score = document.getLong("score") ?: playScore
+                    image = document.getString("image") ?: playImage!!
+                    //v.update("username",newStr)
+                    Log.i("Firebase", username)
+                    Log.i("Firebase", score.toString())
+                    Log.i("Firebase", image)
+
+                    Log.i("Leaderboard", score.toString())
+                    highScores.add(ScoreData(username, score, image))
+                    myAdapter.notifyItemInserted(i++)
+                }
+
+            }
+        }.addOnFailureListener{
+            Log.i("NEWWAY","DEAD")
         }
 
         Log.i("Firebase", "FINAL Count -> " + highScores.size)
@@ -83,6 +140,10 @@ class ScoreboardActivity : AppCompatActivity() {
             fun update(newData : ScoreData){
                 usernameTV.text = newData.userName;
                 scoreTV.text = "Score: " + newData.score;
+                val decoded64 = Base64.decode(newData.imgBaseStr, Base64.DEFAULT)
+                val bitmap = BitmapFactory.decodeByteArray(decoded64,0,decoded64.size)
+                avatarIMG.setImageBitmap(bitmap)
+
                 //avatarIMG.setImageResource( ... )
             }
         }
